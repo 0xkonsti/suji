bd1e500a3de9c8baeb559d9e9eb47756571f8cb1
0xkonsti <opora.privat@gmail.com>
2024-07-23 19:14:26 +0200

    added some bench data

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::io::{stdout, Write};
+
 mod grid;
 mod solver;
 
@@ -5,18 +7,52 @@ const EASY_TEST: &str = "0030206009003050010018064000081029007000000080067082000
 const HARD_TEST: &str = "000000021090700000000000000000514000630000000000002000000600930001040000200000800";
 
 fn main() {
+    let mut stdout = stdout();
+
     let mut sudoku = grid::Grid::new();
-    sudoku.load_from_str(EASY_TEST, '0');
+    // sudoku.load_from_str(EASY_TEST, '0');
     // sudoku.load_from_str(HARD_TEST, '0');
 
-    println!("{}", sudoku);
+    // println!("{}", sudoku);
 
     let mut solver = solver::BruteForceSolver::new();
 
-    let solution = sudoku.solve(&mut solver);
+    // let solution = sudoku.solve(&mut solver);
 
-    match solution {
-        Some(grid) => println!("{}", grid),
-        None => println!("No solution found"),
+    // match solution {
+    //     Some(grid) => println!("{}", grid),
+    //     None => println!("No solution found"),
+    // }
+
+    let easy50 = std::fs::read_to_string("data/easy50.txt")
+        .expect("Unable to open file")
+        .lines()
+        .map(|s| s.to_string())
+        .collect::<Vec<String>>();
+
+    use std::time::Instant;
+    let iter_count = 10;
+    let start = Instant::now();
+
+    for i in 0..iter_count {
+        let sub_start = Instant::now();
+        let mut j = 1;
+        for puzzle in easy50.iter() {
+            sudoku.load_from_str(puzzle, '0');
+            sudoku.solve(&mut solver);
+
+            print!("\r{:<4}/{:>4}", j, easy50.len());
+            stdout.flush().unwrap();
+            j += 1;
+        }
+        println!("\n{}: {:?}", i + 1, sub_start.elapsed());
     }
+
+    let duration = start.elapsed();
+    println!(
+        "Time elapsed in solving 50 puzzles {} times: {:?} | Avg_50: {:?}",
+        iter_count,
+        duration,
+        duration / iter_count
+    );
 }
